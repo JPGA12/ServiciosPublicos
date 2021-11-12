572a2869345ec9ca9fa70e96fc69db9481d3ad15
JUAN <juan.gutierrez12@uptc.edu.co>
2021-11-12 16:34:08 -0500

    Suma

--- main.c
+++ main.c
@@ -4,6 +4,9 @@ int main() {
     printf("Hello, World!\n");
     int numero =10;
     printf("Numero: %i \n ",numero);
+    int numero2 = 2;
+    int suma = numero+numero2;
+    printf("Suma: %i \n",suma);
 
 
     return 0;
