f2c11de845afeedb4f2523aeaba70159dcdcec8e
srr0bot <danielscmo07@gmail.com>
2021-11-12 11:49:13 -0500

    menu

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    puts("Danie gay");
-
-
+    int opc = 0;
+    do {
+        puts("EMPRESA DE SERVICIOS \n 1) FACTURAS AGUA \n 2) FACTURAS ELECTRICIDAD \n 3) SALIR");
+        scanf("%i", &opc);
+        if(opc == 1){
+        }else if(opc == 2){
+        }
+    } while (opc!=3);
     return 0;
 }
