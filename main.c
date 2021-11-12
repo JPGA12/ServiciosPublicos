d1eb1c2fdd0ed65df5318f95bf6b22668538f648
JUAN <juan.gutierrez12@uptc.edu.co>
2021-11-12 16:38:55 -0500

    Iniacial

--- main.c
+++ main.c
@@ -2,11 +2,7 @@
 
 int main() {
     printf("Hello, World!\n");
-    int numero =10;
-    printf("Numero: %i \n ",numero);
-    int numero2 = 2;
-    int suma = numero+numero2;
-    printf("Suma: %i \n",suma);
+    puts("Danie gay");
 
 
     return 0;
